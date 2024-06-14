c8c22ab90961f4f12ac9b516db922a221825e67e
pradhan prasanta <pradhan.prasanta78@gmail.com>
2024-06-15 00:03:39 +0530

    Fix Build faill issue

--- services/nlpservices.py
+++ services/nlpservices.py
@@ -7,7 +7,6 @@ from nltk.tokenize import sent_tokenize
 from heapq import nlargest
 from string import punctuation
 from nltk.stem import PorterStemmer
-import spacy
 import en_core_web_sm
 import string
 
